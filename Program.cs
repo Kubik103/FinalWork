8e9e80f36bda9b7441fd385cb5f997733de8bcc2
det-oj-ord4 <det-oj-ord4@DNServ.local>
2023-04-08 21:49:13 +0300

    Оформление блок-схемы, исправлено. файл task.drawio

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿int [] array1 = new int [] {6,0,10,2,3};
-int [] array2 = new int [3];
+﻿string [] array1 = new string [5] {"hello", "2", "world", ":-)", "123"};
+string [] array2 = new string [3];
+Console.WriteLine (array1);
 
-
+// for (int i=0; i<array2.Length; i++)
+//     {
+//         array2[i]=array1[i];
+//     }
+// Console.WriteLine (array2);
