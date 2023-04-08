ea3ceb67a66a3fb2c53f7a53daa3bc854cff1c89
det-oj-ord4 <det-oj-ord4@DNServ.local>
2023-04-08 22:07:24 +0300

    Написание алгоритма, оформление readme

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 ﻿string [] array1 = new string [5] {"hello", "2", "world", ":-)", "123"};
-string [] array2 = new string [3];
-Console.WriteLine (array1);
+string [] array2 = new string [array1.Length];
 
-// for (int i=0; i<array2.Length; i++)
-//     {
-//         array2[i]=array1[i];
-//     }
-// Console.WriteLine (array2);
+void FillArray2(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray2(array1, array2);
+PrintArray(array2);
